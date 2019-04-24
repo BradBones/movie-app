e7e063e46cb2c7d01c9698e96827bb8b4bfd2a1f
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-24 13:16:53 -0700

    search functionaliy added

--- src/components/elements/HeroImage/HeroImage.js
+++ src/components/elements/HeroImage/HeroImage.js
@@ -4,8 +4,18 @@ import './HeroImage.css';
 // Arrow function - stateless
 const HeroImage = (props) => {
     return (
-        <div>
-            HeroImage
+        <div className="rmdb-heroimage"
+            style={{
+                background:
+                    `linear-gradient(to bottom, rgba(0,0,0,0) 39%, rgba(0,0,0,0) 41%, rgba(0,0,0,0.65) 100%), url('${props.image}'), #c1c1c1`
+            }}
+        >
+            <div className="rmdb-heroimage-content">
+                <div className="rmdb-heroimage-text">
+                    <h1>{props.title}</h1>
+                    <p>{props.text}</p>
+                </div>
+            </div>  
         </div>
     )
 }
--- src/components/elements/SearchBar/SearchBar.js
+++ src/components/elements/SearchBar/SearchBar.js
@@ -5,13 +5,37 @@ import './SearchBar.css';
 // Statefull component
 class SearchBar extends React.Component {
     state = {
-
+        value: ''
     }
     
+    timeout = null;
+
+    doSearch = (event) => {
+        this.setState({ value: event.target.value })
+        clearTimeout(this.timeout);
+
+        this.timeout = setTimeout( () => {
+            this.props.callback(this.state.value)
+        }, 500);
+    }
+
     render() {
         return (
-            <div>
-                SearchBar
+            <div className="rmdb-searchbar">
+                <div className="rmdb-searchbar-content">
+                    <FontAwesome 
+                        className="rmdb-fa-search"
+                        name="search"
+                        size="2x"
+                    />
+                    <input
+                        type="text"
+                        className="rmdb-searchbar-input"
+                        placeholder="search"
+                        onChange={this.doSearch}
+                        value={this.state.value}
+                    />
+                </div>
             </div>
         )
     }
