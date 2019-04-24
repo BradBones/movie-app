723ad5ab87676495e96fa290e7d4ff2bf3ebde09
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-24 10:33:06 -0700

    Building the header

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css';
+
+// Statefull class component (Component imported on line 1)
+class Home extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+}
+
+export default Home;
