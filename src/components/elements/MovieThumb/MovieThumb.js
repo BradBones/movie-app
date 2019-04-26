7d1856f9e5a4c83ddd38815e4260236957a400b2
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-26 10:57:22 -0700

    stage 1 complete

--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './MovieThumb.css';
 
 const MovieThumb = (props) => {
@@ -12,9 +13,14 @@ const MovieThumb = (props) => {
         :
         <img src={props.image} alt="moviethumb" />
       }
-
     </div>
   )
+}
+
+MovieThumb.propTypes = {
+  image: PropTypes.string,
+  movieId: PropTypes.number,
+  movieName: PropTypes.string
 }
 
 export default MovieThumb;
